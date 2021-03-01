02f25c7a43bd8324c9573ccfe69608fbe63c7cf2
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-03-02 00:20:30 +0200

    Refactor norm

--- src/algebra.jl
+++ src/algebra.jl
@@ -1,8 +1,1 @@
-for f in (:+, :-,)
-    @eval function (Base.$f)(f1::AbstractPICDataStructure, f2::AbstractPICDataStructure)
-        @assert f1.grid == f2.grid "Incompatible grids"
-        typeof(f1)(($f).(f1.data, f2.data), f1.grid)
-    end
-end
-
-LinearAlgebra.norm(field::VectorField) = ScalarField(norm.(field), field.grid)
+LinearAlgebra.norm(field::T) where T<:AbstractPICDataStructure = scalar_from(T)(norm.(field.data), field.grid)
