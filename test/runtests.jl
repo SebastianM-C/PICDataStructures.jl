10748c17dfeaabab925c929a1a7063754d563cfb
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-03-03 13:32:00 +0200

    Use correct test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,5 @@ using Test, SafeTestsets
 
 @testset "PICDataStructures.jl" begin
     @safetestset "Scalar quantities" begin include("scalar.jl") end
-    @safetestset "Vector quantities" begin include("scalar.jl") end
+    @safetestset "Vector quantities" begin include("vector.jl") end
 end
