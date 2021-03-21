459205c1e9a4495872a4b615e392a4fc85b6e9da
Documenter.jl <documenter@juliadocs.github.io>
2021-03-21 15:04:30 +0000

    build based on fd061cc

---
+++ v0.3.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PICDataStructures","category":"page"},{"location":"#PICDataStructures","page":"Home","title":"PICDataStructures","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PICDataStructures]","category":"page"},{"location":"#PICDataStructures.find_field-Tuple{Base.Broadcast.Broadcasted}","page":"Home","title":"PICDataStructures.find_field","text":"A = find_filed(Fs) returns the first AbstractPICDataStructure among the arguments.\n\n\n\n\n\n","category":"method"}]
+}
