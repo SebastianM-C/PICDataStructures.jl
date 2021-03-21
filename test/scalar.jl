313f0e51fc0484d801932b6dfb91f163726facb8
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-03-21 15:27:28 +0200

    Fix tests

--- test/scalar.jl
+++ test/scalar.jl
@@ -99,7 +99,7 @@ using Unitful
 end
 
 @testset "Scalar variable interface" begin
-    grid = ParticlePositions((collect(0:0.1:1),), (0.,), (1.,))
+    grid = ParticlePositions((collect(0:0.1:1),))
     data_sets = [
         sin.(first(grid)),
         sin.(first(grid)).*u"V/m",
