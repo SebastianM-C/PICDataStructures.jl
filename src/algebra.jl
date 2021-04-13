914054f62a10bed43246e2bf78d201fdf7d3b903
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-04-13 13:03:39 +0300

    Accept different types
    In case of Unitful fields, the types will be different.
    This also adds a dimensionality check.

--- src/algebra.jl
+++ src/algebra.jl
@@ -5,15 +5,16 @@ function LinearAlgebra.norm(field::T) where T<:AbstractPICDataStructure
     scalar_from(T)(norm.(data), grid)
 end
 
-function LinearAlgebra.cross(a::T, b::T) where T <: AbstractPICDataStructure
+function LinearAlgebra.cross(a::T1, b::T2) where {T1 <: AbstractPICDataStructure, T2 <: AbstractPICDataStructure}
     @assert size(a) == size(b)
-    @assert scalarness(T) === VectorQuantity()
+    @assert scalarness(T1) === scalarness(T2) === VectorQuantity() "The cross product is only defined for Vector quantities"
     @assert propertynames(a) == propertynames(b)
     @assert dimensionality(a) == dimensionality(b) == 3 "The cross product is only defined in 3D"
+
     x_data = @. a.y * b.z - a.z * b.y
     y_data = @. a.z * b.x - a.x * b.z
     z_data = @. a.x * b.y - a.y * b.x
 
     data = StructArray((x_data, y_data, z_data); names=propertynames(a))
-    parameterless_type(T)(data, getdomain(a))
+    parameterless_type(T1)(data, getdomain(a))
 end
