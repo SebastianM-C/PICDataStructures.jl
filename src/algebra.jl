f4b9ae8bcdb9be0f552e1f664a6afb6740b49314
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-04-13 11:05:07 +0300

    Add cross product

--- src/algebra.jl
+++ src/algebra.jl
@@ -4,3 +4,16 @@ function LinearAlgebra.norm(field::T) where T<:AbstractPICDataStructure
 
     scalar_from(T)(norm.(data), grid)
 end
+
+function LinearAlgebra.cross(a::T, b::T) where T <: AbstractPICDataStructure
+    @assert size(a) == size(b)
+    @assert scalarness(T) === VectorQuantity()
+    @assert propertynames(a) == propertynames(b)
+    @assert dimensionality(a) == dimensionality(b) == 3 "The cross product is only defined in 3D"
+    x_data = @. a.y * b.z - a.z * b.y
+    y_data = @. a.z * b.x - a.x * b.z
+    z_data = @. a.x * b.y - a.y * b.x
+
+    data = StructArray((x_data, y_data, z_data); names=propertynames(a))
+    parameterless_type(T)(data, getdomain(a))
+end
