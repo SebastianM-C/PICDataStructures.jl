0d4fd6f173fb0a318c9e5796aab9cc5caddbbe57
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2021-05-02 02:26:23 +0300

    Rename test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using PICDataStructures
 using Test, SafeTestsets
 
 @testset "PICDataStructures.jl" begin
-    @safetestset "Generic tests" begin include("generic.jl") end
+    @safetestset "Code quality" begin include("quality.jl") end
     @safetestset "Grids" begin include("grids.jl") end
     @safetestset "Scalar quantities" begin include("scalar.jl") end
     @safetestset "Vector quantities" begin include("vector.jl") end
